19b58cd8c3c421471d47a174e0cb89e33ae507d5
praveen-me <praveenkrsaini0@gmail.com>
2018-06-20 12:30:02 +0530

    Make Pixel Art Maker Working

--- designs.js
+++ designs.js
@@ -4,23 +4,26 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid (){
+
+
+
+function makeGrid (e){
+    e.preventDefault();
+    //Table grid creation
     //Height
     var height = document.getElementById('inputHeight').value;
-    
+        
     //Width
     var width = document.getElementById('inputWidth').value;
 
     //pixelCanvas
     var table = document.getElementById('pixelCanvas');
-
-    //Table grid creation
     table.innerHTML = '';
 
 
     for(let i = 1; i <= height; i++){
         var tr = document.createElement('tr');
-        for(let j = 0; j <= width; j++){
+        for(let j = 1; j <= width; j++){
             var td = document.createElement('td');
             tr.appendChild(document.createTextNode(''));
             tr.appendChild(td);
@@ -30,13 +33,25 @@ function makeGrid (){
     
 }
 
+var submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', makeGrid);
+
 //Using jQuery to set color
+// $('body').on('click', 'td', function(e){
+//     var colorValue = $('#colorPicker').val();
+//     e.target.style.backgroundColor = colorValue;
+// })
+
+//
+
+// var body = document.querySelector('body');
+// var td = document.getElementsByTagName('td');
+
 $('body').on('click', 'td', function(e){
-    var colorValue = $('#colorPicker').val();
-    e.target.style.backgroundColor = colorValue;
-})
-
-
+	var colorValue = document.getElementById('colorPicker').value;
+  	e.target.style.backgroundColor = colorValue;
+});
 
 
 
