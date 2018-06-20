9c71fb56767ddb3bd16f910d281f022b40dc6666
praveen-me <praveenkrsaini0@gmail.com>
2018-06-20 11:11:47 +0530

    Creating Pixel Art Maker Grid Successfully

---
+++ designs.js
@@ -0,0 +1,36 @@
+
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid (){
+    //Height
+    var height = document.getElementById('inputHeight').value;
+    
+    //Width
+    var width = document.getElementById('inputWidth').value;
+
+    //pixelCanvas
+    var table = document.getElementById('pixelCanvas');
+
+    //Table grid creation
+    table.innerHTML = '';
+
+
+    for(let i = 1; i <= height; i++){
+        var tr = document.createElement('tr');
+        for(let j = 0; j <= width; j++){
+            var td = document.createElement('td');
+            tr.appendChild(document.createTextNode(''));
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);    
+    }
+    
+}
+
+
+
+
+
