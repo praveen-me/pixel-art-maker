66b07a9f827bdd6fd247b45b18c81434ff413734
praveen-me <praveenkrsaini0@gmail.com>
2018-06-22 11:47:27 +0530

    Improve toogle color functionality to a specific td.

--- designs.js
+++ designs.js
@@ -5,49 +5,54 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // When size is submitted by the user, call makeGrid()
 
+//Selects Create Grid Button
+const createGridBtn = $('#createGrid');
 
 
 
-function makeGrid (e){
+
+
+//Select Color Picker
+const colorPicker = $('#colorPicker').val();
+
+//Select Table
+let pixelCanvas = $('#pixelCanvas');
+
+//Rows and Coloums
+
+
+
+//create Grid
+const makeGrid = (e) => {
+    //Selects inputHeight Text Field
+    const inputHeight = $('#inputHeight').val();
+    //Selects inputWidth Text Field
+    const inputWidth = $('#inputWidth').val();
     e.preventDefault();
-    //Table grid creation
-    //Height
-    var height = document.getElementById('inputHeight').value;
-        
-    //Width
-    var width = document.getElementById('inputWidth').value;
-
-    //pixelCanvas
-    var table = document.getElementById('pixelCanvas');
-    table.innerHTML = '';
-
-
-    for(let i = 1; i <= height; i++){
-        var tr = document.createElement('tr');
-        for(let j = 1; j <= width; j++){
-            var td = document.createElement('td');
-            tr.appendChild(document.createTextNode(''));
-            tr.appendChild(td);
+    pixelCanvas.children().remove();
+    for(let i = 1; i <= inputHeight; i++){
+        let row = document.createElement('tr');
+        pixelCanvas.append(row);
+        for(let j = 1; j <= inputWidth; j++){
+            const column = document.createElement('td');
+            row.append(column);
         }
-        table.appendChild(tr);    
+        
     }
-    
 }
-//Make Grid
-const submitButton = document.getElementById('submitButton');
 
-submitButton.addEventListener('click', makeGrid);
+createGridBtn.click(makeGrid);
 
 
-let counter = 0;
-//Set Color to a particular td.
-$('body').on('click', 'td', function(e){
-    counter++;
-	var colorValue = document.getElementById('colorPicker').value;
-    e.target.style.backgroundColor = colorValue;
-    if(counter % 2 === 0) {
-        e.target.style.backgroundColor = 'transparent';
+$('body').on('click','td', function (e) {
+    //Color Value
+    let colorValue = document.getElementById('colorPicker').value;
+    if(e.target.style.backgroundColor == ''){
+        e.target.style.backgroundColor = colorValue;
+    } else {
+        e.target.style.backgroundColor !== 'transparent' ? e.target.style.backgroundColor = 'transparent' : e.target.style.backgroundColor = colorValue;
     }
+
 });
 
 //Reset Grid Color
@@ -57,5 +62,39 @@ reset.addEventListener('click', function(e) {
     const td = document.getElementsByTagName('td');
     $(td).removeAttr('style');
 });
+
+
+//Pure Js Code
+// function makeGrid (e){
+//     e.preventDefault();
+//     //Table grid creation
+//     //Height
+//     var height = document.getElementById('inputHeight').value;
+        
+//     //Width
+//     var width = document.getElementById('inputWidth').value;
+
+//     //pixelCanvas
+//     var table = document.getElementById('pixelCanvas');
+//     table.innerHTML = '';
+
+
+//     for(let i = 1; i <= height; i++){
+//         var tr = document.createElement('tr');
+//         for(let j = 1; j <= width; j++){
+//             var td = document.createElement('td');
+//             tr.appendChild(document.createTextNode(''));
+//             tr.appendChild(td);
+//         }
+//         table.appendChild(tr);    
+//     }
+    
+// }
+// //Make Grid
+// const submitButton = document.getElementById('submitButton');
+
+// submitButton.addEventListener('click', makeGrid);
+
+
 
 });
